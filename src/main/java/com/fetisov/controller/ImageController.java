eb2d53ae500f972bccff5c42e3191c2146bf9afc
fetisov <fetisov@sibers.ru>
2022-05-23 19:47:29 +0700

    Adding the Note entity Making one to one connection Adding the realisation of using only one DB for Notes Looking for FrontEnd. Try to finish today

--- src/main/java/com/fetisov/controller/ImageController.java
+++ src/main/java/com/fetisov/controller/ImageController.java
@@ -1,61 +1,62 @@
-package com.fetisov.controller;
-import com.fetisov.entities.Image;
-import com.fetisov.repositories.ImageRepository;
-import com.fetisov.services.ImageServiceImp;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-import java.util.Optional;
-
-@Validated
-@RestController
-//@CrossOrigin(origins = "http://localhost:8082") open for specific port
-public class ImageController {
-
-   @Autowired
-    private  ImageRepository imageRepository;
-
-   @Autowired
-    private static ImageServiceImp imageServiceImp;
-
-    Image image = new Image();
-
-    @PostMapping("/upload/image")
-    public ResponseEntity<ImageUploadResponse> uploadImage(@RequestParam("image") MultipartFile file)
-            throws IOException {
-
-        image.setName(file.getOriginalFilename());
-        image.setType(file.getContentType());
-        image.setImage(ImageServiceImp.compressImage(file.getBytes()));
-        image.setId(image.getId());
-
-
-        imageRepository.save(image);
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new ImageUploadResponse("Image uploaded successfully: " +
-                        file.getOriginalFilename()));
-    }
-
-  @GetMapping(path = {"/get/image/info/{name}"})
- public Image getImageDetails(@PathVariable("name") String name) throws IOException {
-        Optional<Image> imageInfo = imageRepository.findImageByName(name);
-        return imageInfo.get();
- }
-
-   @GetMapping(path = {"/get/image/{name}"})
- public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
-
-     final Optional<Image> dbImage = imageRepository.findByName(name);
-
-       return ResponseEntity
-               .ok()
-               .contentType(MediaType.valueOf(dbImage.get().getType()))
-               .body(ImageServiceImp.decompressImage(dbImage.get().getImage()));
- }
-}
+//package com.fetisov.controller;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Validated
+//@RestController
+////@CrossOrigin(origins = "http://localhost:8082") open for specific port
+//public class ImageController {
+//
+//   @Autowired
+//    private  ImageRepository imageRepository;
+//
+//   @Autowired
+//    private static ImageServiceImp imageServiceImp;
+//
+//    Image image = new Image();
+//
+//    @PostMapping("/upload")
+//    public ResponseEntity<ImageUploadResponse> uploadImage(@RequestParam("image") MultipartFile file)
+//            throws IOException {
+//
+//        image.setName(file.getOriginalFilename());
+//        image.setImage(ImageServiceImp.compressImage(file.getBytes()));
+//        image.setId(image.getId());
+//        imageRepository.save(image);
+//
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(new ImageUploadResponse("Image uploaded successfully: " +
+//                        file.getOriginalFilename()));
+//    }
+//
+//  @GetMapping(path = {"/get/image/info/{name}"})
+// public Image getImageDetails(@PathVariable("name") String name) throws IOException {
+//        Optional<Image> imageInfo = imageRepository.findImageByName(name);
+//        return imageInfo.get();
+// }
+//
+//   @GetMapping(path = {"/get/image/{name}"})
+// public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+//     final Optional<Image> dbImage = imageRepository.findByName(name);
+//       return ResponseEntity
+//               .ok()
+//               .contentType(MediaType.valueOf(dbImage.get().getName()))
+//               .body(ImageServiceImp.decompressImage(dbImage.get().getImage()));
+//
+// }
+// @GetMapping(path = {"/mainPage"})
+//    public ResponseEntity<byte[]> getAllImages(@PathVariable("name") String name) throws  IOException{
+//        List<Image> allImages =  imageRepository.findAll();
+//        return (ResponseEntity<byte[]>) allImages;
+// }
+//
+//}
--- src/main/java/com/fetisov/repositories/UploadImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fetisov.repositories;
-
-import com.fetisov.entities.UploadImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadImageRepository extends JpaRepository<UploadImageEntity, String> {
-}
